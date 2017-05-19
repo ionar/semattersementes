01c12b13ae9b835edc45000c69734bc2ddc1a89a
Ionar <irm@ionar.com.br>
2017-05-19 15:05:24 -0300

    fix paperclip instalation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Adicionado para a gem paperclip - Ionar
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
