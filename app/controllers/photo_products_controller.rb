6d1c298d64723d0d60ac581fa7b7856103b89539
Ionar <irm@ionar.com.br>
2017-07-27 14:36:13 -0300

    display photos

--- app/controllers/photo_products_controller.rb
+++ app/controllers/photo_products_controller.rb
@@ -1,2 +1,15 @@
 class PhotoProductsController < ApplicationController
+	def create
+		@product = Product.find(params[:product_id])
+		permitted_columns = params[:photo_product].permit(:photo, :product_id)
+		@photo_product = @product.photo_products.create(permitted_columns)
+		redirect_to edit_product_path(@product)
+	end
+
+	def destroy
+		@product = Product.find(params[:product_id])
+		@photo_product = @product.photo_products.find(params[:id])
+		@photo_product.destroy
+		redirect_to edit_product_path(@product)
+	end
 end
